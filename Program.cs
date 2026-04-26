4f5347bbee44b986db9d0060b7097ea070d02a58
skellybob56 <lucas.chowney@outlook.com>
2026-04-26 23:10:43 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,135 @@
+﻿
+using System.Diagnostics;
+using System.Xml;
+
+namespace Tokito;
+
+internal static class Program
+{
+    // todo: add options for how lossy encoding should be
+    // todo: improve naming (Encode and PairEncode are confusing names to mean completely different things)
+    static byte[] Encode(string text, string[] words, char[] punctuation)
+    {
+        static byte ParseCurrentWord(string currentWord, string[] words)
+        {
+            // todo: add safety for cases where the word is unknown
+            return (byte) words.IndexOf(currentWord);
+        }
+
+        if (words.Length + punctuation.Length > byte.MaxValue)
+        { throw new ArgumentException("The current format does not allow for more than 256 total words and punctuation", nameof(words) + ", " + nameof(punctuation)); }
+
+        List<byte> tokens = [];
+        
+        string currentWord = "";
+        foreach (char character in text)
+        {
+            if (character >= 'a' && character <= 'z')
+            {
+                currentWord += character;
+            }
+            else
+            {
+                if (currentWord != "")
+                {
+                    tokens.Add(ParseCurrentWord(currentWord, words));
+                    currentWord = "";
+                }
+                if (punctuation.Contains(character))
+                {
+                    // todo: add safety for cases where the character is unknown
+                    tokens.Add((byte)(words.Length + punctuation.IndexOf(character)));
+                }
+            }
+        }
+        if (currentWord != "")
+        { tokens.Add(ParseCurrentWord(currentWord, words)); }
+
+        return tokens.ToArray();
+    }
+
+    static string Decode(byte[] tokens, string[] words, char[] punctuation)
+    {
+        string output = "";
+
+        foreach (byte index in tokens)
+        {
+            // todo: add safety to ensure that the index isn't greater than the length of punctuation and words combined
+            // todo: add automatic spacing to words and punctuation. this can be done by having each punctuation mark also store an enum for what spacing it uses.
+            output += index < words.Length ? words[index] : punctuation[index - words.Length];
+            output += ' '; // temp spacer
+        }
+
+        return output;
+    }
+
+    static (byte[] tokens, (byte p1, byte p2)[] pairs) PairEncode(byte[] tokens, int maximumPairCount)
+    {
+        // todo: implement pseudocode (this currently just applies no encoding)
+
+        List<(byte p1, byte p2)> pairs = [];
+        LinkedList<byte> linkedTokens = new(tokens);
+
+        while (true)
+        {
+            // if there are no spaces for additional pairs: break the loop
+            if (pairs.Count == maximumPairCount) { break; }
+            Debug.Assert(pairs.Count <= maximumPairCount);
+
+            // find the most common adjacent token pair
+
+            // if the most common pair's frequency is less than three: break the loop
+            if (true)
+            {
+                break;
+            }
+
+            // add the pair to a pair list and replace all instances of the pair in the tokens array
+        }
+
+        return (linkedTokens.ToArray(), pairs.ToArray());
+    }
+
+    static byte[] GenerateHeader((byte p1, byte p2)[] pairs)
+    {
+        if (pairs.Length > byte.MaxValue)
+        { throw new ArgumentException("The current format does not allow for more than 255 pairs", nameof(pairs)); }
+        byte[] header = new byte[1 + 2*pairs.Length];
+        header[0] = (byte)pairs.Length;
+
+        for (int i = 0; i < pairs.Length; i++)
+        {
+            header[1 + 2*i] = pairs[i].p1;
+            header[2 + 2*i] = pairs[i].p2;
+        }
+
+        return header;
+    }
+
+    static byte[] Compress(byte[] tokens, int maximumPairCount)
+    {
+        (tokens, (byte p1, byte p2)[] pairs) = PairEncode(tokens, maximumPairCount);
+        byte[] header = GenerateHeader(pairs);
+
+        byte[] compressed = new byte[header.Length + tokens.Length];
+        header.CopyTo(compressed, 0);
+        tokens.CopyTo(compressed, header.Length);
+
+        return compressed;
+    }
+
+    static void Main()
+    {
+        // todo: load these from data files
+        string text = "nasin lete\n\nlon tenpo lete la jan mute li tawa kepeken ilo suli.\ntaso mi tawa kepeken noka. mi jo ala e ilo tawa suli. mi sona ala kepeken ilo tawa suli.\nko walo li kama tan sewi la jan pali pi kulupu lawa ma li weka e ko walo tan nasin ilo kepeken tenpo lili. ike la, jan pali sama li weka ala e ko tan nasin pi tawa noka.\nmi jan tawa nanpa wan la mi o pali e nasin kepeken noka mi a!\ntaso tenpo mute la mi jan tawa nanpa wan ala. mi ken tawa lon nasin ni: jan ante li tawa lon tenpo pini.\nmi pilin pona tan ni: jan ante mute li sama mi li tawa noka lon tenpo lete. kulupu lawa li pali sama ni: mi lon ala. taso mi ale li lon li awen tawa kepeken noka a!\n";
+        string[] words = ["a", "akesi", "ala", "alasa", "ale", "anpa", "ante", "anu", "awen", "e", "en", "esun", "ijo", "ike", "ilo", "insa", "jaki", "jan", "jelo", "jo", "kala", "kalama", "kama", "kasi", "ken", "kepeken", "kili", "kiwen", "ko", "kon", "kule", "kulupu", "kute", "la", "lape", "laso", "lawa", "len", "lete", "li", "lili", "linja", "lipu", "loje", "lon", "luka", "lukin", "lupa", "ma", "mama", "mani", "mi", "moku", "moli", "monsi", "mu", "mun", "musi", "mute", "nanpa", "nasa", "nasin", "nena", "ni", "nimi", "noka", "o", "olin", "ona", "open", "pakala", "pali", "palisa", "pan", "pana", "pi", "pilin", "pimeja", "pini", "pipi", "poka", "poki", "pona", "pu", "sama", "seli", "selo", "seme", "sewi", "sijelo", "sike", "sin", "sina", "sinpin", "sitelen", "sona", "soweli", "suli", "suno", "supa", "suwi", "tan", "taso", "tawa", "telo", "tenpo", "toki", "tomo", "tu", "unpa", "uta", "utala", "walo", "wan", "waso", "wawa", "weka", "wile"];
+        char[] punctuation = ['\n', '.', ',', ':', '"', '?', '!', '\''];
+
+        byte[] tokens = Encode(text, words, punctuation);
+        byte[] compressed = Compress(tokens, 256 - (words.Length + punctuation.Length));
+
+        Console.WriteLine(Decode(tokens, words, punctuation));
+
+        Console.Read(); // pause until enter
+    }
+}
---
+++ plan/plan.md
@@ -0,0 +1,19 @@
+# theory
+
+## tokenisation
+This will require a word dictionary and a hard-coded set of punctuation. The reason that the set is hard-coded is because many punctuation marks requrie special code and they are rather standard. It would be ideal to make the punctuation system data-driven. Possibly, in the future, I could add a byte that acts to slice the (frequency ordered) punctuation set down to a smaller set allowing potentially greater compression when encoding basic punctuation at the cost of an extra header byte.
+
+The text will be assumed to be made only of punctuation, whitespace and letters. Any instance of an uppercase letter will be assumed to be the start of a proper noun. For now, proper nouns will be encoded simply in ascii but in the future, I hope that I will be able to detect when I can incode these as toki pona syllables which will allow for greater compression. ASCII will be encoded by having a specific internal punctuation that means "start ascii string" and by using the ASCII end of string character to end it.
+If a document written using only these characters is encoded, it will be decoded identically to the original with merely two exceptions. Capital letters that aren't at the start of words will be removed. Secondly, double spaces or spaces in other strange locations, e.g. before a comma, will be removed as spaces are implied through context. In the future, these could be fixed by detecting aberrant situations and encoding the oddity with ASCII. Furthermore, in the future, it'd be great to detect any ASCII character that is not supported and encode it raw too. This could also be extended to allow for Unicode characters.
+
+From here, what we can do is scan every character while encoding the file. If the character is a punctuation point, the 'current token' (prior, unaccounted for string) is appended to tokens (after some sanitisation) and the punctuation mark is appended to tokens. Certain punctuation points have custom functions such as the space not appending itself to. Else, if the character is a capital, mark the current token as a name. If it is a capital or not, concatenate the char to the end of the current token.
+
+## function sections
+Text
+Tokens
+Pairs + Tokens
+Header + Bytes
+
+## far future possibilities
+Use non-byte-aligned compression such as Huffman encoding or even non-bit-aligned encoding such as arithmetic encoding.
+Use an understanding of toki pona grammar to improve compression.
