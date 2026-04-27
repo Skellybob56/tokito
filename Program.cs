eca551382d5dd3b0ef55d00f42dc7901ddcd84d7
skellybob56 <lucas.chowney@outlook.com>
2026-04-27 09:44:31 +0100

    Improve naming of previous Encode/Decode functions for clarity

--- Program.cs
+++ Program.cs
@@ -7,8 +7,7 @@ namespace Tokito;
 internal static class Program
 {
     // todo: add options for how lossy encoding should be
-    // todo: improve naming (Encode and PairEncode are confusing names to mean completely different things)
-    static byte[] Encode(string text, string[] words, char[] punctuation)
+    static byte[] Tokenize(string text, string[] words, char[] punctuation)
     {
         static byte ParseCurrentWord(string currentWord, string[] words)
         {
@@ -48,7 +47,7 @@ internal static class Program
         return tokens.ToArray();
     }
 
-    static string Decode(byte[] tokens, string[] words, char[] punctuation)
+    static string Detokenize(byte[] tokens, string[] words, char[] punctuation)
     {
         string output = "";
 
@@ -125,10 +124,10 @@ internal static class Program
         string[] words = ["a", "akesi", "ala", "alasa", "ale", "anpa", "ante", "anu", "awen", "e", "en", "esun", "ijo", "ike", "ilo", "insa", "jaki", "jan", "jelo", "jo", "kala", "kalama", "kama", "kasi", "ken", "kepeken", "kili", "kiwen", "ko", "kon", "kule", "kulupu", "kute", "la", "lape", "laso", "lawa", "len", "lete", "li", "lili", "linja", "lipu", "loje", "lon", "luka", "lukin", "lupa", "ma", "mama", "mani", "mi", "moku", "moli", "monsi", "mu", "mun", "musi", "mute", "nanpa", "nasa", "nasin", "nena", "ni", "nimi", "noka", "o", "olin", "ona", "open", "pakala", "pali", "palisa", "pan", "pana", "pi", "pilin", "pimeja", "pini", "pipi", "poka", "poki", "pona", "pu", "sama", "seli", "selo", "seme", "sewi", "sijelo", "sike", "sin", "sina", "sinpin", "sitelen", "sona", "soweli", "suli", "suno", "supa", "suwi", "tan", "taso", "tawa", "telo", "tenpo", "toki", "tomo", "tu", "unpa", "uta", "utala", "walo", "wan", "waso", "wawa", "weka", "wile"];
         char[] punctuation = ['\n', '.', ',', ':', '"', '?', '!', '\''];
 
-        byte[] tokens = Encode(text, words, punctuation);
+        byte[] tokens = Tokenize(text, words, punctuation);
         byte[] compressed = Compress(tokens, 256 - (words.Length + punctuation.Length));
 
-        Console.WriteLine(Decode(tokens, words, punctuation));
+        Console.WriteLine(Detokenize(tokens, words, punctuation));
 
         Console.Read(); // pause until enter
     }
