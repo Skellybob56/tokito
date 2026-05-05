4e79efd0707344498486150910b0a28b3308965a
skellybob56 <lucas.chowney@outlook.com>
2026-05-05 21:04:45 +0100

    Update LogicalTokens to include a SpaceSupressor and an ExplicitSpaceToken. Also implement Spaced() and Word() methods for LogicalToken and the SerializeableToken subset.

--- LogicalTokens.cs
+++ LogicalTokens.cs
@@ -1,14 +1,32 @@
 
-using System.Text;
-
 namespace Tokito;
 
 static partial class TokiCodex
 {
-	abstract record LogicalToken;
-	record WordToken(int WordIndex) : LogicalToken;
-	record PunctuationToken(int PunctuationIndex) : LogicalToken;
-	record CharToken(char Value) : LogicalToken;
-	
+	abstract record LogicalToken
+	{
+		// if automatically spaces itself from a following word
+		public bool Spaced() => this switch
+		{
+			WordToken => true,
+			PunctuationToken pt => punctuation[pt.PunctuationIndex].spaced,
+			_ => false
+		};
+
+		// this will become useful as a toki pona syllable string will also be considered a word here
+		public bool Word() => this switch
+		{
+			WordToken => true,
+			_ => false
+		};
+	}
+	record ExplicitSpaceToken : LogicalToken;
+
+	abstract record SerializableToken : LogicalToken;
+	record WordToken(int WordIndex) : SerializableToken;
+	record PunctuationToken(int PunctuationIndex) : SerializableToken;
+	record CharToken(char Value) : SerializableToken;
+	record SpaceSupressor : SerializableToken;
+
 	// todo: add token(s) for toki pona syllables
 }
