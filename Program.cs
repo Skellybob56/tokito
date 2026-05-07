41e67ba11a88a9b813811b9a7afd9b329f7e79d2
skellybob56 <lucas.chowney@outlook.com>
2026-05-07 10:34:33 +0100

    Remove calls to ByteCodex.Compress because the compression mistakenly was not aware of escape strings

--- Program.cs
+++ Program.cs
@@ -15,14 +15,11 @@ internal static class Program
 
 		// todo: future - remove debugging and messy code
 		byte[] tokens = TokiCodex.Encode(text);
-		byte[] compressed = ByteCodex.Compress((byte[])tokens.Clone(), TokiCodex.minimumPairIndex);
 
-		File.WriteAllBytes($"{textPath}.toki", compressed);
+		File.WriteAllBytes($"{textPath}.toki", tokens);
 
 		Console.WriteLine("Done!");
 
-		byte[] decompressed = ByteCodex.Decompress(compressed, TokiCodex.minimumPairIndex);
-		Debug.Assert(decompressed.SequenceEqual(tokens), "Decompressed data should be equivalent to the original tokens");
 		string decoded = TokiCodex.Decode(tokens, useCRLF);
 		Debug.Assert(decoded == text, "Untokenized tokens should be equivalent to the original text");
 
