af414f4115023b2d5140a36cceb9937e0ceeeba7
skellybob56 <lucas.chowney@outlook.com>
2026-05-14 09:54:20 +0100

    Split some types out from PairEncode.cs into PairTypes.cs

--- PairEncode.cs
+++ PairEncode.cs
@@ -6,36 +6,6 @@ namespace Tokito;
 
 static partial class TokiCodex
 {
-	enum EscapeTag : byte
-	{
-		Token,
-		SyllableString,
-		AsciiString,
-		Utf16String
-	}
-
-	readonly record struct TaggedByte(EscapeTag Tag, byte Value);
-
-	readonly record struct BytePair(byte Pair1, byte Pair2)
-	{
-		public int ToIndex()
-		{
-			return Pair1 * 256 + Pair2;
-		}
-
-		public static int ToIndex(byte Pair1, byte Pair2)
-		{
-			return Pair1 * 256 + Pair2;
-		}
-
-		public static BytePair FromIndex(int index)
-		{
-			return new((byte)(index >> 8), (byte)index);
-		}
-	}
-
-	readonly record struct TaggedBytePair(EscapeTag Tag, BytePair Pair);
-
 	readonly record struct FirstPassData(
 		LinkedList<TaggedByte> TaggedTokens,
 		uint[] TokenPairFrequency,
