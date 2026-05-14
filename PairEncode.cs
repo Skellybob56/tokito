fd429a0f4f99eb4b7a5f542f3fb642d1faebd2d3
skellybob56 <lucas.chowney@outlook.com>
2026-05-14 09:41:42 +0100

    Expand BytePair to have to/from index methods

--- PairEncode.cs
+++ PairEncode.cs
@@ -16,7 +16,23 @@ static partial class TokiCodex
 
 	readonly record struct TaggedByte(EscapeTag Tag, byte Value);
 
-	readonly record struct BytePair(byte Pair1, byte Pair2);
+	readonly record struct BytePair(byte Pair1, byte Pair2)
+	{
+		public int ToIndex()
+		{
+			return Pair1 * 256 + Pair2;
+		}
+
+		public static int ToIndex(byte Pair1, byte Pair2)
+		{
+			return Pair1 * 256 + Pair2;
+		}
+
+		public static BytePair FromIndex(int index)
+		{
+			return new((byte)(index >> 8), (byte)index);
+		}
+	}
 
 	readonly record struct TaggedBytePair(EscapeTag Tag, BytePair Pair);
 
